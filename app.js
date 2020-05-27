0c7279a0cda8bb5c5deb2e918bac84899cd4b4c8
lin <1640677966@qq.com>
2020-05-27 10:49:02 +0800

    提交了初始的代码

---
+++ app.js
@@ -0,0 +1,14 @@
+const cors = require('cors');
+const express = require('express');
+const app = express();
+app.listen(3007, () => console.log('大事件服务器启动了'));
+
+// 配置中间件
+app.use(cors());
+app.use(express.urlencoded({extended: false}));
+
+// 加载路由模块
+app.use('/api', require(path.join(__dirname, 'routers', 'login')));
+app.use('/my/article', require(path.join(__dirname, 'routers', 'category')));
+app.use('/my/article', require(path.join(__dirname, 'routers', 'article')));
+app.use('/my', require(path.join(__dirname, 'routers', 'user')));
